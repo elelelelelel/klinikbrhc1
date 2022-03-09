a50fc95e814fed6350ec7b2cb1c62756c9548943
El <94628041+elelelelelel@users.noreply.github.com>
2022-03-09 20:33:01 +0700

    Add files via upload

---
+++ company.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace q1.Pages.q1
+{
+    public class companyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
